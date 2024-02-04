410697ca34226edafb24dd409fd0a88715462a43
megamilkx <gyrocopter3k@gmail.com>
2024-02-04 22:55:30 +0300

    Gui stuff

---
+++ src/common/gui/elements/menu_list.hpp
@@ -0,0 +1,185 @@
+#pragma once
+
+#include "gui/gui_system.hpp"
+#include "gui/elements/element.hpp"
+#include "gui/gui_icon.hpp"
+
+
+class GuiMenuList;
+class GuiMenuListItem : public GuiElement {
+    GuiTextBuffer caption;
+    bool is_open = false;
+    std::unique_ptr<GuiMenuList> menu_list;
+    GuiIcon* icon_arrow = 0;
+public:
+    std::function<void(void)> on_click;
+
+    int id = 0;
+    int command_identifier = 0;
+    void* user_ptr = 0;
+
+    void open();
+    void close();
+
+    bool hasList() { return menu_list.get() != nullptr; }
+
+    GuiMenuListItem(const char* cap, std::function<void(void)> on_click)
+        : on_click(on_click) {
+        setSize(0, 0);
+        caption.replaceAll(getFont(), cap, strlen(cap));
+        icon_arrow = guiLoadIcon("svg/entypo/triangle-right.svg");
+    }
+    GuiMenuListItem(const char* cap = "MenuListItem", int cmd = 0)
+        : command_identifier(cmd) {
+        setSize(0, 0);
+        caption.replaceAll(getFont(), cap, strlen(cap));
+        icon_arrow = guiLoadIcon("svg/entypo/triangle-right.svg");
+    }
+    GuiMenuListItem(const char* cap, const std::initializer_list<GuiMenuListItem*>& child_items);
+    bool onMessage(GUI_MSG msg, GUI_MSG_PARAMS params) {
+        switch (msg) {
+        case GUI_MSG::CLOSE_MENU:
+            return true;
+        case GUI_MSG::MOUSE_ENTER:
+            notifyOwner(GUI_NOTIFY::MENU_ITEM_HOVER, id);
+            break;
+        case GUI_MSG::LCLICK:
+        case GUI_MSG::DBL_LCLICK:
+            if (hasList()) {
+                open();
+                //notifyOwner(GUI_NOTIFY::MENU_ITEM_CLICKED, id);
+            } else {
+                notifyOwner(GUI_NOTIFY::MENU_COMMAND, command_identifier);
+                if (on_click) {
+                    on_click();
+                }
+            }
+            return true;
+        case GUI_MSG::NOTIFY:
+            switch (params.getA<GUI_NOTIFY>()) {
+            case GUI_NOTIFY::MENU_COMMAND:
+                close();
+                forwardMessageToOwner(msg, params);
+                return true;
+            }
+            break;
+        }
+
+        return GuiElement::onMessage(msg, params);
+    }
+    void onLayout(const gfxm::rect& rc, uint64_t flags) override {
+        Font* font = getFont();
+        caption.prepareDraw(font, false);
+        const float h = font->getLineHeight() * 2.0f;
+        const float w = gfxm::_max(rc.max.x - rc.min.x, caption.getBoundingSize().x + GUI_MARGIN * 2.f);
+        rc_bounds = gfxm::rect(rc.min, gfxm::vec2(rc.min.x + w, rc.min.y + h));
+        client_area = rc_bounds;
+    }
+    void onDraw() override {
+        Font* font = getFont();
+        if (isHovered()) {
+            guiDrawRect(client_area, GUI_COL_BUTTON);
+        }
+        {
+            gfxm::rect rc = client_area;
+            rc.min.x += GUI_MARGIN;
+            caption.draw(font, rc, GUI_LEFT | GUI_VCENTER, GUI_COL_TEXT, GUI_COL_ACCENT);
+            //caption.draw(client_area.min + gfxm::vec2(GUI_MARGIN, guiGetCurrentFont()->font->getLineHeight() * .25f), GUI_COL_TEXT, GUI_COL_ACCENT);
+        }
+        float fontH = font->getLineHeight();
+        if (hasList() && icon_arrow) {
+            icon_arrow->draw(guiLayoutPlaceRectInsideRect(client_area, gfxm::vec2(fontH, fontH), GUI_RIGHT | GUI_VCENTER, gfxm::rect(GUI_MARGIN, GUI_MARGIN, GUI_MARGIN, GUI_MARGIN)), GUI_COL_TEXT);
+        }
+    }
+};
+class GuiMenuList : public GuiElement {
+    std::vector<std::unique_ptr<GuiMenuListItem>> items;
+    GuiMenuListItem* open_elem = 0;
+public:
+    void open() {
+        setHidden(false);
+    }
+    void close() {
+        setHidden(true);
+        if (open_elem) {
+            open_elem->close();
+        }
+    }
+
+    GuiMenuList() {
+        addFlags(
+            GUI_FLAG_TOPMOST
+            | GUI_FLAG_FLOATING
+            | GUI_FLAG_MENU_POPUP
+        );
+        overflow = GUI_OVERFLOW_FIT;
+    }
+    GuiMenuList* addItem(GuiMenuListItem* item) {
+        item->id = items.size();
+        items.push_back(std::unique_ptr<GuiMenuListItem>(item));
+        addChild(item);
+        item->setOwner(this);
+        return this;
+    }
+    bool onMessage(GUI_MSG msg, GUI_MSG_PARAMS params) override {
+        switch (msg) {
+        case GUI_MSG::CLOSE_MENU:
+            if (getOwner()) {
+                guiPostMessage(getOwner(), GUI_MSG::CLOSE_MENU, GUI_MSG_PARAMS());
+            }
+            setHidden(true);
+            return true;
+        case GUI_MSG::NOTIFY:
+            switch (params.getA<GUI_NOTIFY>()) {
+            case GUI_NOTIFY::MENU_ITEM_HOVER: {
+                int id = params.getB<int>();
+                if (items[id]->hasList()) {
+                    if (open_elem && open_elem->id != params.getA<int>()) {
+                        open_elem->close();
+                        open_elem = nullptr;
+                    }
+                    if (!open_elem && items[id]->hasList()) {
+                        open_elem = items[id].get();
+                        open_elem->open();
+                    }
+                }
+                }return true;
+            }
+            break;
+        }
+        return false;
+    }
+    void onDraw() override {
+        guiDrawRectShadow(rc_bounds);
+        guiDrawRect(rc_bounds, GUI_COL_HEADER);
+        guiDrawRectLine(rc_bounds, GUI_COL_BUTTON);
+        for (int i = 0; i < childCount(); ++i) {
+            auto ch = getChild(i);
+            ch->draw();
+        }
+    }
+};
+inline GuiMenuListItem::GuiMenuListItem(const char* cap, const std::initializer_list<GuiMenuListItem*>& child_items) {
+    setSize(0, 0);
+    caption.replaceAll(getFont(), cap, strlen(cap));
+    menu_list.reset(new GuiMenuList);
+    menu_list->setOwner(this);
+    menu_list->setHidden(true);
+    menu_list->addFlags(GUI_FLAG_MENU_SKIP_OWNER_CLICK);
+    guiGetRoot()->addChild(menu_list.get());
+    for (auto ch : child_items) {
+        menu_list->addItem(ch);
+    }
+    icon_arrow = guiLoadIcon("svg/entypo/triangle-right.svg");
+}
+inline void GuiMenuListItem::open() {
+    menu_list->open();
+    menu_list->pos = gui_vec2(client_area.max.x, client_area.min.y);
+    menu_list->size = gui_vec2(200, 200);
+    is_open = true;
+    guiBringWindowToTop(menu_list.get());
+}
+inline void GuiMenuListItem::close() {
+    menu_list->close();
+    is_open = false;
+}
---
+++ src/common/gui/elements/state_graph.hpp
@@ -0,0 +1,176 @@
+#pragma once
+
+#include "gui/elements/element.hpp"
+
+
+
+class GuiAnimStateNode : public GuiElement {
+public:
+    GuiAnimStateNode() {
+        setPosition(0, 0);
+        setSize(250, 50);
+        addFlags(GUI_FLAG_FLOATING);
+        overflow = GUI_OVERFLOW_NONE;
+    }
+    bool onMessage(GUI_MSG msg, GUI_MSG_PARAMS params) {
+        switch (msg) {
+        case GUI_MSG::PULL_START:
+        case GUI_MSG::PULL_STOP: {
+            return true;
+        }
+        case GUI_MSG::PULL: {
+            pos.x.value += params.getA<float>();
+            pos.y.value += params.getB<float>();
+            return true;
+        }
+        case GUI_MSG::LCLICK: {
+            notifyOwner(GUI_NOTIFY::STATE_NODE_CLICKED, this, 0);
+            return true;
+        }
+        case GUI_MSG::DBL_LCLICK: {
+            notifyOwner(GUI_NOTIFY::STATE_NODE_DOUBLE_CLICKED, this, 0);
+            return true;
+        }
+        case GUI_MSG::MOVING: {
+            gfxm::rect* prc = params.getB<gfxm::rect*>();
+            gfxm::vec2 to = gfxm::vec2(prc->max.x, prc->max.y);
+            gfxm::vec2 from = gfxm::vec2(prc->min.x, prc->min.y);
+            gfxm::vec2 diff = to - from;
+            // TODO: FIX UNITS
+            pos.x.value += diff.x;
+            pos.y.value += diff.y;
+            return true;
+        }
+        }
+        return false;
+    }
+    void onDraw() override {
+        gfxm::vec2 shadow_offset(10.0f, 10.0f);
+        guiDrawRectRoundBorder(gfxm::rect(
+            rc_bounds.min + shadow_offset, rc_bounds.max + shadow_offset
+        ), 15.0f, 10.0f, 0x00000000, 0xAA000000);
+        guiDrawRectRound(rc_bounds, 15, GUI_COL_BUTTON);
+        guiDrawText(client_area, "AnimStateNode", getFont(), GUI_HCENTER | GUI_VCENTER, GUI_COL_TEXT);
+    }
+};
+class GuiStateGraph : public GuiElement {
+    struct Connection {
+        GuiAnimStateNode* from;
+        GuiAnimStateNode* to;
+    };
+
+    GuiAnimStateNode* connection_preview_src = 0;
+    std::vector<Connection> connections;
+
+    bool connectionExists(GuiAnimStateNode* a, GuiAnimStateNode* b) {
+        for (int i = 0; i < connections.size(); ++i) {
+            auto& c = connections[i];
+            if (c.from == a && c.to == b) {
+                return true;
+            }
+        }
+        return false;
+    }
+    bool makeConnection(GuiAnimStateNode* a, GuiAnimStateNode* b) {
+        if (connectionExists(a, b)) {
+            return false;
+        }
+        connections.push_back(Connection{ a, b });
+        return true;
+    }
+public:
+    GuiStateGraph() {
+        addFlags(GUI_FLAG_DRAG_CONTENT);
+        setSize(0, 0);
+
+        setStyleClasses({ "state-graph" });
+
+        {
+            auto ctx_menu = new GuiMenuList();
+            ctx_menu->addItem(new GuiMenuListItem("Hello"));
+            ctx_menu->addItem(new GuiMenuListItem("World!"));
+            ctx_menu->addItem(new GuiMenuListItem("Foo"));
+            ctx_menu->addItem(new GuiMenuListItem("Bar"));
+            guiAddContextPopup(this, ctx_menu);
+        }
+        auto node = new GuiAnimStateNode;
+        node->setPosition(100, 100);
+        guiAdd(this, this, node);
+
+        node = new GuiAnimStateNode;
+        node->setPosition(150, 280);
+        guiAdd(this, this, node);
+
+        node = new GuiAnimStateNode;
+        node->setPosition(250, 220);
+        guiAdd(this, this, node);
+    }
+    bool onMessage(GUI_MSG msg, GUI_MSG_PARAMS params) override {
+        switch (msg) {
+        case GUI_MSG::RCLICK:
+        case GUI_MSG::DBL_RCLICK: {
+            if (connection_preview_src) {
+                connection_preview_src = 0;
+                return true;
+            }
+            break;
+        }
+        case GUI_MSG::NOTIFY: {
+            switch (params.getA<GUI_NOTIFY>()) {
+            case GUI_NOTIFY::STATE_NODE_CLICKED: {
+                if (connection_preview_src) {
+                    auto node_b = params.getB<GuiAnimStateNode*>();
+                    if (node_b == connection_preview_src) {
+                        return true;
+                    }
+                    makeConnection(connection_preview_src, node_b);
+                    connection_preview_src = 0;
+                }
+                return true;
+            }
+            case GUI_NOTIFY::STATE_NODE_DOUBLE_CLICKED: {
+                connection_preview_src = params.getB<GuiAnimStateNode*>();
+                return true;
+            }
+            }
+            break;
+        }
+        }
+        return GuiElement::onMessage(msg, params);
+    }
+    void onDraw() override {
+        for (int i = 0; i < connections.size(); ++i) {
+            auto& c = connections[i];
+            gfxm::vec2 pta = (c.from->getBoundingRect().min + c.from->getBoundingRect().max) * .5f;
+            gfxm::vec2 ptb = (c.to->getBoundingRect().min + c.to->getBoundingRect().max) * .5f;
+            gfxm::vec2 offs = gfxm::normalize(ptb - pta);
+            std::swap(offs.x, offs.y);
+            offs *= 10.f;
+            offs.y = -offs.y;
+            guiDrawLineWithArrow(
+                pta + offs,
+                ptb + offs,
+                5.f, GUI_COL_TEXT
+            );
+        }
+        if (connection_preview_src) {
+            gfxm::rect rc_node = connection_preview_src->getBoundingRect();
+            gfxm::vec2 ptsrc = (rc_node.min + rc_node.max) * .5f;
+            gfxm::vec2 mouse = guiGetMousePos();
+            gfxm::vec2 offs = gfxm::normalize(mouse - ptsrc);
+            std::swap(offs.x, offs.y);
+            offs *= 10.f;
+            offs.y = -offs.y;
+            guiDrawLineWithArrow(
+                ptsrc + offs,
+                mouse,
+                5.f, GUI_COL_TIMELINE_CURSOR
+            );
+        }
+
+        GuiElement::onDraw();
+
+        guiDrawText(client_area.min, "Double click on a node - start new connection", getFont(), 0, GUI_COL_TEXT);
+        guiDrawText(client_area.min + gfxm::vec2(.0f, 20.f), MKSTR("Pos content: " << pos_content.x << " " << pos_content.y).c_str(), getFont(), 0, GUI_COL_TEXT);
+    }
+};
