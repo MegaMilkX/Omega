49ec699c2638048e26b25d96e17c5942a044a7c9
MegaMilkX <gyrocopter3k@gmail.com>
2026-06-20 17:22:53 +0300

    Remove unused

--- src/common/log/log.cpp
+++ src/common/log/log.cpp
@@ -27,71 +27,7 @@ void Log::AddConsumer(LogConsumer* c) {
 
 
 Log::Log()
-: working(true) {
-    /*
-    thread_writer = std::thread([this](){
-        tm ptm = {0};
-        time_t t = time(0);
-        localtime_s(&ptm, &t);
-        char buffer[64];
-        strftime(buffer, 64, "%d%m%Y", &ptm);
-        std::string fname = win32GetThisModuleName() + "_" + std::string(buffer);
-
-        fsCreateDirRecursive(fsGetModuleDir() + "\\log");
-
-        //std::ios::openmode mode = std::ios::out | std::ios::app;
-        std::ios::openmode mode = std::ios::out | std::ios::trunc;
-        std::ofstream f(fsGetModuleDir() + "\\log\\" + fname + ".log", mode);
-
-        do {
-            std::queue<LogEntry> lines_copy;
-            {
-                std::lock_guard<std::mutex> lock(sync);
-                if(!working && lines.empty())
-                    break;
-                
-                lines_copy = lines;
-                while(!lines.empty()) {
-                    lines.pop();
-                }
-            }
-            while(!lines_copy.empty()) {
-                LogEntry e = lines_copy.front();
-                tm ptm = {0};
-                localtime_s(&ptm, &e.t);
-                char buffer[32];
-                strftime(buffer, 32, "%H:%M:%S", &ptm); 
-                lines_copy.pop();
-
-                HANDLE  hConsole;	
-                hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-                if(e.type == LOG_TYPE::LOG_INFO) {
-                    SetConsoleTextAttribute(hConsole, 7);
-                } else if(e.type == LOG_TYPE::LOG_WARN) {
-                    SetConsoleTextAttribute(hConsole, 0xE);
-                } else if(e.type == LOG_TYPE::LOG_ERROR) {
-                    SetConsoleTextAttribute(hConsole, 0xC);
-                } else if(e.type == LOG_TYPE::LOG_DEBUG_INFO) {
-                    SetConsoleTextAttribute(hConsole, 0x0001 | 0x0002 | 0x0008);
-                }
-                std::string str = static_cast<std::ostringstream&>(
-                    std::ostringstream() << logTypeToString(e.type) 
-                    << " | " << buffer 
-                    << " | " << std::hex << std::uppercase << e.thread_id 
-                    << ": " << e.line 
-                    << std::endl).str();
-                f << str;
-                std::cout << str;
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        } while(1);
-
-        f << "\n\n\n";
-
-        f.flush();
-        f.close();
-    });*/
-    
+: working(true) {    
     stdout_consumer.reset(new StdoutLogConsumer());
     _addConsumer(stdout_consumer.get());
     file_consumer.reset(new FileLogConsumer());
